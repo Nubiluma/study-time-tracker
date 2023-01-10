1bddd505cc075d17bf6f6dbd75eabbaad8cf1ae5
Sandra Petereit <sandrap3393@googlemail.com>
2023-01-10 20:05:25 +0100

    add notification | edit save timer function

--- script.js
+++ script.js
@@ -7,12 +7,13 @@ class Timer {
   }
 }
 
-const timerSeconds = document.getElementById("timerSeconds");
-const timerMinutes = document.getElementById("timerMinutes");
-const timerHours = document.getElementById("timerHours");
-const startBtn = document.getElementById("btnStartTimer");
-const pauseBtn = document.getElementById("btnPauseTimer");
-const resetBtn = document.getElementById("btnResetTimer");
+const timerSeconds = document.querySelector("#timerSeconds");
+const timerMinutes = document.querySelector("#timerMinutes");
+const timerHours = document.querySelector("#timerHours");
+const startBtn = document.querySelector("#btnStartTimer");
+const pauseBtn = document.querySelector("#btnPauseTimer");
+const resetBtn = document.querySelector("#btnResetTimer");
+const main = document.querySelector("main");
 
 let currentInterval;
 
@@ -25,6 +26,7 @@ const state = {
 
 startBtn.addEventListener("click", startTimer);
 pauseBtn.addEventListener("click", pauseTimer);
+resetBtn.addEventListener("click", resetTimer);
 
 getDataFromLocalStorage();
 renderTimer();
@@ -33,7 +35,7 @@ renderTimer();
 
 function startTimer() {
   if (state.activeTimer == null) {
-    state.activeTimer = new Timer(0, 0, 0, Date());
+    state.activeTimer = new Timer(0, 0, 0, formatDate(new Date()));
   }
   currentInterval = setInterval(() => {
     state.activeTimer.seconds++;
@@ -53,18 +55,98 @@ function startTimer() {
 
 function pauseTimer() {
   clearInterval(currentInterval);
-
-  if (
-    state.savedTimers.length === 0 ||
-    state.savedTimers[0].date !== state.activeTimer.date
-  ) {
-    state.savedTimers.unshift(state.activeTimer);
-  } else {
-    state.savedTimers[0] = state.activeTimer;
-  }
-  console.log(state.savedTimers);
   updateLocalStorage();
 }
+
+function resetTimer() {
+  if (state.activeTimer != null) {
+    pauseTimer();
+    demandUserFeedback();
+  }
+}
+
+function saveTimer() {
+  if (state.savedTimers.length === 0 || state.savedTimers[0] === null) {
+    state.savedTimers.unshift(state.activeTimer);
+    updateLocalStorage();
+  } else if (state.savedTimers[0].date !== state.activeTimer.date) {
+    state.savedTimers.unshift(state.activeTimer);
+    updateLocalStorage();
+  } else if (state.savedTimers[0].date === state.activeTimer.date) {
+    state.savedTimers[0] = accountTimers(
+      state.activeTimer,
+      state.savedTimers[0]
+    );
+    updateLocalStorage();
+  }
+}
+
+function accountTimers(activeTimer, savedTimer) {
+  let sumSeconds = savedTimer.seconds + activeTimer.seconds;
+  let sumMinutes = savedTimer.minutes + activeTimer.minutes;
+  let sumHours = savedTimer.hours + activeTimer.hours;
+
+  if (sumSeconds < 120 && sumMinutes < 120 && sumHours < 24) {
+    if (sumSeconds >= 60) {
+      sumSeconds -= 60;
+      sumMinutes++;
+    }
+    if (sumMinutes >= 60) {
+      sumMinutes -= 60;
+      sumHours++;
+    }
+    savedTimer.seconds = sumSeconds;
+    savedTimer.minutes = sumMinutes;
+    savedTimer.hours = sumHours;
+
+    console.log(savedTimer);
+    return savedTimer;
+  } else {
+    console.error(
+      "timer(s) invalid: seconds, minutes or hours exceed limit of 60 and/or 24!"
+    );
+  }
+}
+
+function demandUserFeedback() {
+  const notification = document.createElement("aside");
+  notification.id = "notification";
+  const msg = document.createTextNode(
+    "Do you want to save the timer's progress since last started?"
+  );
+  notification.appendChild(msg);
+  const saveBtn = document.createElement("button");
+  const confirmBtnTxt = document.createTextNode("Save");
+  saveBtn.appendChild(confirmBtnTxt);
+  const dismissBtn = document.createElement("button");
+  const dismissBtnTxt = document.createTextNode("Dismiss");
+  dismissBtn.appendChild(dismissBtnTxt);
+  const cancelBtn = document.createElement("button");
+  const cancelBtnTxt = document.createTextNode("Cancel");
+  cancelBtn.appendChild(cancelBtnTxt);
+
+  saveBtn.addEventListener("click", saveAction);
+  dismissBtn.addEventListener("click", dismissAction);
+  cancelBtn.addEventListener("click", cancelAction);
+
+  notification.appendChild(saveBtn);
+  notification.appendChild(dismissBtn);
+  notification.appendChild(cancelBtn);
+
+  main.appendChild(notification);
+}
+
+function saveAction() {
+  saveTimer();
+  state.activeTimer = null;
+  renderTimer();
+  const notification = document.querySelector("#notification");
+  notification.remove();
+}
+
+function dismissAction() {}
+
+function cancelAction() {}
 
 /*********************************************************************/
 
@@ -85,6 +167,10 @@ function renderTimer() {
     } else {
       timerHours.innerText = state.activeTimer.hours;
     }
+  } else {
+    timerSeconds.innerText = "00";
+    timerMinutes.innerText = "00";
+    timerHours.innerText = "00";
   }
 }
 
@@ -104,4 +190,8 @@ function getDataFromLocalStorage() {
 function updateLocalStorage() {
   localStorage.setItem("activeTimer", JSON.stringify(state.activeTimer));
   localStorage.setItem("savedTimers", JSON.stringify(state.savedTimers));
+}
+
+function formatDate(date) {
+  return date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
 }
