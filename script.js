7c4cc3496bd52d54cb2d6cd1f255de09db7f1537
Sandra Petereit <sandrap3393@googlemail.com>
2023-01-11 11:02:05 +0100

    disable timer buttons when reset event is active

--- script.js
+++ script.js
@@ -61,15 +61,20 @@ function pauseTimer() {
 function resetTimer() {
   if (state.activeTimer != null) {
     pauseTimer();
-    demandUserFeedback();
+    createNotificationForUserFeedback();
   }
 }
 
+/**
+ * save active timer to saved timers in localStorage
+ * overwrite first saved timer object if active timer has same date, otherwise add as new element to array at index 0
+ */
 function saveTimer() {
   if (state.savedTimers.length === 0 || state.savedTimers[0] === null) {
     state.savedTimers.unshift(state.activeTimer);
     updateLocalStorage();
   } else if (state.savedTimers[0].date !== state.activeTimer.date) {
+    createNotificationForUserFeedback();
     state.savedTimers.unshift(state.activeTimer);
     updateLocalStorage();
   } else if (state.savedTimers[0].date === state.activeTimer.date) {
@@ -81,6 +86,12 @@ function saveTimer() {
   }
 }
 
+/**
+ * account seconds, minutes and hours of two timer objects
+ * @param {*} activeTimer active timer
+ * @param {*} savedTimer saved timer from array
+ * @returns accounted sum of activeTimer and savedTimer as savedTimer
+ */
 function accountTimers(activeTimer, savedTimer) {
   let sumSeconds = savedTimer.seconds + activeTimer.seconds;
   let sumMinutes = savedTimer.minutes + activeTimer.minutes;
@@ -108,11 +119,18 @@ function accountTimers(activeTimer, savedTimer) {
   }
 }
 
-function demandUserFeedback() {
+/**
+ * create markup for user to decide what to do with active timer
+ * let user choose to save, dismiss or cancel decision
+ */
+function createNotificationForUserFeedback() {
+  startBtn.disabled = true;
+  pauseBtn.disabled = true;
+  resetBtn.disabled = true;
   const notification = document.createElement("aside");
   notification.id = "notification";
   const msg = document.createTextNode(
-    "Do you want to save the timer's progress since last started?"
+    "Do you want to save the timer's progress?"
   );
   notification.appendChild(msg);
   const saveBtn = document.createElement("button");
@@ -136,6 +154,10 @@ function demandUserFeedback() {
   main.appendChild(notification);
 }
 
+/**
+ * save-button clicked
+ * save changes of active timer to storage and reset active timer
+ */
 function saveAction() {
   saveTimer();
   state.activeTimer = null;
@@ -143,19 +165,36 @@ function saveAction() {
   renderTimer();
   const notification = document.querySelector("#notification");
   notification.remove();
+  startBtn.disabled = false;
+  pauseBtn.disabled = false;
+  resetBtn.disabled = false;
 }
 
+/**
+ * dismiss-button clicked
+ * reset active timer and do not save changes to storage
+ */
 function dismissAction() {
   state.activeTimer = null;
   updateLocalStorage();
   renderTimer();
   const notification = document.querySelector("#notification");
   notification.remove();
+  startBtn.disabled = false;
+  pauseBtn.disabled = false;
+  resetBtn.disabled = false;
 }
 
+/**
+ * cancel-button clicked
+ * do not do anything with active timer, only delete notification markup element from dom
+ */
 function cancelAction() {
   const notification = document.querySelector("#notification");
   notification.remove();
+  startBtn.disabled = false;
+  pauseBtn.disabled = false;
+  resetBtn.disabled = false;
 }
 
 /*********************************************************************/
